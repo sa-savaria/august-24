70b74508fa0f3280c38c08f68f31fd37e8f08609
sa-savaria <69935806+sa-savaria@users.noreply.github.com>
2020-08-24 18:02:48 -0400

    Update practice-api-script.js

--- docs/practice-api-script.js
+++ docs/practice-api-script.js
@@ -21,6 +21,45 @@ $.ajax({
 });
 }
 
-function displayResults(res){
+function displayResults(results){	
+// display the results of the query
 
+// get the div where the results will be shown
+var showdiv = document.getElementById("resultsdiv");
+	
+// empty that div of its current contents	
+showdiv.innerHTML = "";
+var titleInfo, title, authorInfo,author; // create a variable or two
+// cycle through each of the elements of the results array	
+for (i=0; i< results.item.length; i++){	
+// get the author	
+authorInfo = results.item[i].contributor;	
+// if there was no author key, provide some helpful content
+if (authorInfo == undefined){	
+authorInfo = "[No author listed]";	
+}	
+// is it an array?	
+if ($.isArray(authorInfo)){	
+author = authorInfo.join("; ");	
+}	
+else { // if it's not an Array
+author = authorInfo;	
+}	
+// get the title	
+titleInfo = results.item[i].title;	
+// if there was no author key, provide some helpful content	
+if (titleInfo == undefined){	
+titleInfo = "[No title listed]";	
+}	
+// is it an array?	
+if ($.isArray(titleInfo)){	
+title = titleInfo.join(" - ");	
+}	
+else { // if it's not an Array	
+title = titleInfo;	
+}
+// create a new div for the title and add it to the page	
+var currentcontent = showdiv.innerHTML;	
+showdiv.innerHTML = currentcontent + "<div class='oneresult'><span class='titleclass'>" + title + "</span>, by <span class='authorclass'>" + author + "</span></div>";
+}	
 }
